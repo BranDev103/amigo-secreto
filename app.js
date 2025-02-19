6d117eb75cacdd9023c85872a326f83b812357e0
Brandon GL <brandonglopez.103@gmail.com>
2025-02-18 18:41:32 -0600

    funcion para sortear amigo

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ function agregaAmigo() {
 /*********************************************************************************************************************/
 
 //FUNCION PARA MOSTRAR NOMBRES AGREGADOS (ACTUALIZAR LISTA)
-function actualizarLista(){
+function actualizarLista() {
     let listaAmigos = document.getElementById("listaAmigos"); //Se obtiene el elemento lista
     listaAmigos.innerHTML = ""; //Se limpia la lista 
 
-    for(let i = 0; i<amigos.length; i++){ //Se recorre el arreglo y se crean los elementos de la lista
+    for (let i = 0; i < amigos.length; i++) { //Se recorre el arreglo y se crean los elementos de la lista
         let nuevoElemento = document.createElement("li");
         nuevoElemento.textContent = amigos[i];
         listaAmigos.appendChild(nuevoElemento);
@@ -46,3 +46,21 @@ function actualizarLista(){
     return;
 }
 
+/*********************************************************************************************************************/
+
+//FUNCION PARA SORTEAR AMIGO
+function sortearAmigo() {
+
+    listaAmigos.innerHTML = "";//Se limpia la lista
+
+    if (amigos.length > 0) {//Ciclo para recorrer el arreglo
+
+        let amigoAleatorio = Math.floor(Math.random() * amigos.length);//Se sortea el amigo
+        let amigoSorteado = amigos[amigoAleatorio];
+        document.getElementById("resultado").textContent = amigoSorteado;//Se muestra en pantalla el amigo sorteado
+
+    } else {
+        alert("No hay amigos para sortear");
+    }
+    return;
+}
